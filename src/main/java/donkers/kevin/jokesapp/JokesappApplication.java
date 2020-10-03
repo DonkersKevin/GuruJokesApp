fcd12c7301f3cdc2a0d90f35ab3e9a0cf31a38bc
DonkersKevin <Donkers.Kevin86@gmail.com>
2020-10-03 14:26:58 +0200

    first commit

---
+++ src/main/java/donkers/kevin/jokesapp/JokesappApplication.java
@@ -0,0 +1,13 @@
+package donkers.kevin.jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesappApplication.class, args);
+    }
+
+}
