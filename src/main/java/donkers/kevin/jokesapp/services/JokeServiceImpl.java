c0e13b47f2f7475b19fc8be572b2423627686353
DonkersKevin <Donkers.Kevin86@gmail.com>
2020-10-16 13:20:04 +0200

    Added java config file, and config XML as 2 options

--- src/main/java/donkers/kevin/jokesapp/services/JokeServiceImpl.java
+++ src/main/java/donkers/kevin/jokesapp/services/JokeServiceImpl.java
@@ -1,6 +1,7 @@
 package donkers.kevin.jokesapp.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 
@@ -9,8 +10,8 @@ public class JokeServiceImpl implements JokeService{
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
